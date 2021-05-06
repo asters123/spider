34e51f227d0fa62929ddfc807d99b4ed9ad638fd
asters1 <GJ744559140@163.com>
2021-05-06 16:55:39 +0800

    重启防火墙脚本

---
+++ restart_firewall/auto_restart_firewall.py
@@ -0,0 +1,35 @@
+#!/bin/python3
+
+import requests
+#User-Agent: Mozilla/5.0 (X11; Linux x86_64) AppleWebKit/537.36 (KHTML, like Gecko) Chrome/86.0.4240.183 Safari/537.36
+if __name__ == "__main__":
+    url = "http://10.0.0.1"
+    headers = {
+        "User-Agent":"Mozilla/5.0 (X11; Linux x86_64) AppleWebKit/537.36 (KHTML, like Gecko) Chrome/86.0.4240.183 Safari/537.36"
+    }
+    data_login = {
+        'luci_username':'root',
+        'luci_password':'000000'
+            }
+    session = requests.Session()
+    session.post(url=url,headers=headers,data=data_login)
+    session.get()
+#token: 661abd68fd77afc1fc3c18b1d45ee552
+#cbi.submit: 1
+#cbid.firewall.1._custom: # This file is interpreted as shell script.
+#
+## Put your custom iptables rules here, they will
+#
+## be executed with each firewall (re-)start.
+#
+#
+#
+## Internal uci firewall chains are flushed and recreated on reload, so
+#
+## put custom rules into the root chains e.g. INPUT or FORWARD or into the
+#
+## special user chains, e.g. input_wan_rule or postrouting_lan_rule.
+#
+#iptables -t nat -A PREROUTING -p udp --dport 53 -j REDIRECT --to-ports 53
+#
+#iptables -t nat -A PREROUTING -p tcp --dport 53 -j REDIRECT --to-ports 53
